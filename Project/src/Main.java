a6dd9db21a0ae3283af00a7bacab502b6c1f9d1a
Local Uploader <>
2021-12-02 20:42:57 +0530

    updated java code for fx

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,5 +1,21 @@
-public class Main{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application{
+	@Override
+	public void start (Stage stage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Menu.fxml"));
+		Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+
+		stage.setTitle("Hello!");
+		stage.setScene(scene);
+		stage.show();
+	}
 	public static void main (String[] args) {
-		System.out.println("Hello world");
+		launch();
 	}
 }
