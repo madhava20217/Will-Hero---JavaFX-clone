842677226d867d0c66b4ae086bff6945ed3034ae
Local Uploader <>
2021-12-05 19:03:35 +0530

    added images, modified playing screen fxml.

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main extends Application{
 	@Override
 	public void start (Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("PlayScreen.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("templates/Menu.fxml"));
 		Scene scene = new Scene(fxmlLoader.load(), 1048, 590);
 
 		stage.setTitle("Hello!");
