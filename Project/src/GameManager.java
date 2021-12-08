ba2196e24c0bd259c17f489e3a73abae47b2eb18
Local Uploader <>
2021-12-08 14:14:53 +0530

    fixed controller class. works properly now

--- Project/src/GameManager.java
+++ Project/src/GameManager.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public class GameManager extends Application implements Serializable {
-	GameManager(){
+	public GameManager(){
 	}
 	
 	@Override
